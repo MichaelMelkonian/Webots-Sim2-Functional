14940366ef6d98d0981e6cc5ae6cf1d1990f1b96
Michael Melkonian <mikemel20@gmail.com>
2022-02-06 20:44:02 -0800

    First Committ

---
+++ controllers/front_command/build/release/front_command.d
@@ -0,0 +1,5 @@
+build/release/front_command.o: front_command.cpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/mikem/AppData/Local/Programs/Webots/include/controller/c/webots/types.h
---
+++ controllers/obstacle_Avoid/build/release/obstacle_Avoid.d
@@ -0,0 +1,6 @@
+build/release/obstacle_Avoid.o: obstacle_Avoid.cpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/mikem/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\mikem\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp
